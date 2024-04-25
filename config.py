09feb701ad0e32d3adafddc859dfb2fb9ed08601
xiongfei <xiongf@citics.com>
2024-04-25 17:00:21 +0800

    提交

--- config.py
+++ config.py
@@ -5,5 +5,6 @@ DEBUG = True
 
 # 读取数据库环境变量
 username = os.environ.get("MYSQL_USERNAME", 'root')
-password = os.environ.get("MYSQL_PASSWORD", 'root')
+# password = os.environ.get("MYSQL_PASSWORD", 'root')
+password = 'Xf216613'
 db_address = os.environ.get("MYSQL_ADDRESS", '127.0.0.1:3306')
