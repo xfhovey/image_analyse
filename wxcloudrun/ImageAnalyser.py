e01ca6a7d658591dfa516728a70bde5a63c5f4b7
xiongfei <xiongf@citics.com>
2024-04-26 11:24:21 +0800

    增加处理类

---
+++ wxcloudrun/ImageAnalyser.py
@@ -0,0 +1,151 @@
+import cv2
+import numpy as np
+from PIL import Image, ImageDraw, ImageFont
+from pyparsing import *
+
+from ocrengin import get_ocr_engins
+
+COLOR_BLUE = (255, 0, 0)
+COLOR_GREEN = (0, 255, 0)
+COLOR_RED = (0, 0, 255)
+COLOR_WHITE = (255, 255, 255)
+COLOR_BLACK = (0, 0, 0)
+COLOR_GRAY = (127, 127, 127)
+
+
+def draw_text(image, x, y, text, color=COLOR_RED):
+    pilimg = Image.fromarray(image)
+    draw = ImageDraw.Draw(pilimg)
+    # 参数1：字体文件路径，参数2：字体大小
+    font = ImageFont.truetype("simhei.ttf", 30, encoding="utf-8")
+    # 参数1：打印坐标，参数2：文本，参数3：字体颜色，参数4：字体
+    draw.text((x, y - 10), text, color, font=font)
+    return cv2.cvtColor(np.array(pilimg), cv2.COLOR_RGB2BGR)
+
+
+# 定义数字
+integer = Word(nums)
+# 定义比较符
+operator = oneOf("> < =")
+# 定义基本的数学运算符
+math_op = oneOf("+ - * / × ÷")
+# 定义表达式
+expr = Forward()
+atom = integer | Group(Literal("(").suppress() + expr + Literal(")").suppress())
+expr <<= atom + ZeroOrMore(math_op + atom)
+original_expr = originalTextFor(expr)
+# 定义整个等式或不等式的语法
+equation = original_expr.setResultsName('left_side') + operator.setResultsName('oper') + original_expr.setResultsName(
+    'right_side')
+
+
+def parse_equation(equation_str):
+    try:
+        # 尝试解析输入的字符串
+        parsed = equation.parseString(equation_str)
+        left_side = parsed.left_side.replace('×', '*').replace('÷', '/')
+        right_side = parsed.right_side.replace('×', '*').replace('÷', '/')
+        return left_side, parsed.oper, right_side
+    except ParseException:
+        # 如果解析失败，返回None
+        return None
+
+
+class ImageAnalyser:
+
+    def __init__(self, alpha=1.5, beta=0, new_size=(640, 480), thresh=127, engin='baidu'):
+        self.alpha = alpha
+        self.beta = beta
+        self.new_size = new_size
+        self.thresh = thresh
+        self.engin = engin
+        self.gray_image = None
+        self.bgr_image = None
+
+    def _get_engin(self):
+        support_engins = get_ocr_engins()
+        if self.engin not in support_engins:
+            return support_engins['baidu']()
+        return support_engins[self.engin]()
+
+    def prev_deal(self, image_path):
+        image = cv2.imread(image_path)
+
+        # 调节对比度
+        adjusted_image = cv2.convertScaleAbs(image, alpha=self.alpha, beta=self.beta)
+
+        # 插值
+        # # 最近邻插值
+        # image_nearest = cv2.resize(image, new_size, interpolation=cv2.INTER_NEAREST)
+        #
+        # # 双线性插值
+        # image_linear = cv2.resize(image, new_size, interpolation=cv2.INTER_LINEAR)
+
+        # 双三次插值
+        image_cubic = cv2.resize(adjusted_image, self.new_size, interpolation=cv2.INTER_CUBIC)
+
+        # 展示原始图像和缩放后的图像
+        # cv2.imshow('Original Image', image)
+        # cv2.imshow('Nearest Interpolation', image_cubic)
+
+        # Convert to grayscale and apply threshold
+        gray = cv2.cvtColor(image_cubic, cv2.COLOR_BGR2GRAY)
+        _, binary_image = cv2.threshold(gray, self.thresh, 255, cv2.THRESH_BINARY)  # + cv2.THRESH_OTSU
+        self.gray_image = binary_image
+        return binary_image
+
+    def analyse(self, image_path):
+
+        ocr_engin = self._get_engin()
+        if self.engin == "baidu":
+            results = ocr_engin.identify(image_path)
+        else:
+            binary_image = self.prev_deal(image_path=image_path)
+            results = ocr_engin.identify(binary_image)
+
+        color_image = cv2.imread(image_path)
+        correct_num = 0
+        wrong_num = 0
+        for _, row in results.iterrows():
+            text = row['text']
+            (x, y, w, h) = row['left'], row['top'], row['width'], row['height']
+
+            result = parse_equation(text)
+            if result:
+                left_side, oper, right_side = result
+                left_result = int(eval(left_side))
+                right_result = int(eval(right_side))
+                if ((oper == '=' and left_result == right_result)
+                        or (oper == '<' and left_result < right_result)
+                        or (oper == '>' and left_result > right_result)):
+                    x_end, y_end = x + w, y + h // 2
+                    color_image = draw_text(color_image, x_end, y_end, '√', color=COLOR_RED)
+                    correct_num += 1
+                else:
+                    x_end, y_end = x + w, y + h // 2
+                    color_image = draw_text(color_image, x_end, y_end, '×', color=COLOR_BLUE)
+                    cv2.rectangle(color_image, (x, y), (x + w, y + h), (0, 255, 0), 1)
+                    color_image = draw_text(color_image, x, y, text, color=COLOR_GREEN)
+                    wrong_num += 1
+
+        self.bgr_image = color_image
+        return correct_num, wrong_num
+
+    def to_path(self, image_path, gray_or_bgr='bgr'):
+        if gray_or_bgr == 'bgr':
+            image = self.bgr_image
+        else:
+            image = self.gray_image
+
+        cv2.imwrite(image_path, image)
+        return image_path
+
+
+if __name__ == '__main__':
+    image_path = r'image\o4-Hhv4x_F3G-Vk3Q_1713936209.jpg'
+    analyser = ImageAnalyser()
+    analyser.analyse(image_path)
+    cv2.imshow('image', analyser.bgr_image)
+
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
---
+++ wxcloudrun/weixin.py
@@ -0,0 +1,104 @@
+import datetime
+import hashlib
+import os
+from urllib import request
+
+import requests
+
+
+def HttpRequest(resp_func, logger=None):
+    def check_result(func):
+        def wrapper(*args, **kwargs):
+            response = func(*args, **kwargs)
+            if response.status_code != 200:
+                logger.info(response) if logger else print(response)
+                return None
+            res = response.json()
+            print(res)
+            if 'errcode' in res and res["errcode"] != 0:
+                logger.info(res) if logger else print(res)
+                return None
+
+            return resp_func(res)
+
+        return wrapper
+
+    return check_result
+
+
+class WeixinAdaptor:
+    def __init__(self, logger=None):
+        self.appid = 'wxd8150c294734a1d1'  # 'wx2ff607a6d3522e27'
+        self.app_secret = '6198d462c3c09ae6f21c73b6eb093f77'  # 'af53e3a54f64bced04453d11af24b982'
+        self.token = '1234567890'
+        self._access_token = None
+        self._access_token_period = None
+        self.logger = logger
+
+    @property
+    def access_token(self):
+        if self._access_token is None or self._accesstoken_timeout():
+            token_info = self._get_token()
+            if token_info is None:
+                return None
+            self._access_token, expires_in = token_info
+            self._access_token_period = datetime.datetime.now() + datetime.timedelta(seconds=expires_in)
+            self.logger.info(self._access_token)
+        return self._access_token
+
+    def _accesstoken_timeout(self):
+        return self._access_token_period is None \
+               or datetime.datetime.now() > self._access_token_period
+
+    def validate(self, signature, echostr, timestamp, nonce):
+        lst = [self.token, timestamp, nonce]
+        lst.sort()
+        sha1 = hashlib.sha1()
+        sha1.update(lst[0].encode("utf-8"))
+        sha1.update(lst[1].encode("utf-8"))
+        sha1.update(lst[2].encode("utf-8"))
+        hashcode = sha1.hexdigest()
+
+        if hashcode == signature:
+            return echostr
+        else:
+            return ""
+
+    @HttpRequest(resp_func=lambda resp: (resp['access_token'], resp['expires_in']))
+    def _get_token(self):
+        url = 'https://api.weixin.qq.com/cgi-bin/token'
+        params = {
+            'grant_type': 'client_credential',
+            'appid': self.appid,
+            'secret': self.app_secret,
+        }
+        response = requests.get(url, params=params)
+        return response
+
+    def get_image(self, imageMsg, local_dir):
+        fromUser = imageMsg.FromUserName[0:8]
+        mediaId = imageMsg.MediaId[0:8]
+        picUrl = imageMsg.PicUrl
+        createTime = imageMsg.CreateTime
+        image_name = f'{fromUser}_{mediaId}_{createTime}.jpg'
+        image_path = os.path.join(local_dir, image_name)
+        request.urlretrieve(picUrl, image_path)
+        return image_path
+
+    @HttpRequest(resp_func=lambda resp: resp['media_id'])
+    def upload_image(self, image_path):
+        url = f'https://api.weixin.qq.com/cgi-bin/media/upload?access_token={self.access_token}&type=image'
+
+        with open(image_path, 'rb') as f:
+            files = {'media': f}
+            response = requests.post(url, files=files)
+        return response
+
+
+    @HttpRequest(resp_func=lambda resp: resp['errcode'])
+    def send_msg(self, replyMsg):
+        url = f'https://api.weixin.qq.com/cgi-bin/message/custom/send?access_token={self.access_token}'
+        headers = {'content-Type': 'application/json'}
+        response = requests.post(url, headers=headers, data=replyMsg.to_json())
+        return response
+
